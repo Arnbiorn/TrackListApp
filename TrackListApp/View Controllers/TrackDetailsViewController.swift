92b8cf35d6274a751b97960243bae9e4bf14d455
Arnbiorn <skrutelev988@gmail.com>
2020-11-08 19:24:18 +0300

    Configure track detail view controller

--- TrackListApp/View Controllers/TrackDetailsViewController.swift
+++ TrackListApp/View Controllers/TrackDetailsViewController.swift
@@ -9,7 +9,16 @@ import UIKit
 
 class TrackDetailsViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet var artCoverImageView: UIImageView!
+    @IBOutlet var trackLabel: UILabel!
+    
+    // MARK: - Public Properties
+    var track: Track!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        artCoverImageView.image = UIImage(named: track.track)
+        trackLabel.text = track.track
     }
 }
