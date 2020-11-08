a4f21f5e635e125b9aef5424a53903df855e54bd
Arnbiorn <skrutelev988@gmail.com>
2020-11-08 18:49:30 +0300

    Add TrackListViewController

---
+++ TrackListApp/View Controllers/TrackListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TrackListViewController.swift
+//  TrackListApp
+//
+//  Created by Егор Скрутелев on 08.11.2020.
+//
+
+import UIKit
+
+class TrackListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
