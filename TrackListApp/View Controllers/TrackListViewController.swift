cd9180e20b8ff27a0810657acc46ec905de6f0b4
Arnbiorn <skrutelev988@gmail.com>
2020-11-08 19:50:11 +0300

    Configure main view controller in app

--- TrackListApp/View Controllers/TrackListViewController.swift
+++ TrackListApp/View Controllers/TrackListViewController.swift
@@ -8,8 +8,59 @@
 import UIKit
 
 class TrackListViewController: UITableViewController {
+    
+    var trackList = Track.getTrackList()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = 80
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let trackDetailVC = segue.destination as! TrackDetailsViewController
+        trackDetailVC.track = sender as? Track
+    }
+}
+
+// MARK: - Table view data source
+extension TrackListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        trackList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "trackName", for: indexPath)
+        
+        let track = trackList[indexPath.row]
+        cell.textLabel?.text = track.song
+        cell.textLabel?.numberOfLines = 0
+        cell.detailTextLabel?.text = track.artist
+        cell.imageView?.image = UIImage(named: track.track)
+        
+        return cell
+    }
+}
+
+// MARK: - Table view delegate
+extension TrackListViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let track = trackList[indexPath.row]
+        performSegue(withIdentifier: "showDetails", sender: track)
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        .none
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        false
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let currentTrack = trackList.remove(at: sourceIndexPath.row)
+        trackList.insert(currentTrack, at: destinationIndexPath.row)
     }
 }
